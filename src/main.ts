5b2f0e269f9de89ae85f8a6115dee79bf94deeae
Roberto Carlos Salas Valencia <roberto.cs.valencia@gmail.com>
2024-04-14 17:10:48 -0500

    chore: adding helmet, compression, global validator and enabling cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import * as compression from "compression";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.use(compression());
+  app.use(helmet());
+  app.enableCors({
+    origin: "*",
+  });
   await app.listen(3000);
 }
 
