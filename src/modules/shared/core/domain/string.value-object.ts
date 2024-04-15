752995efac2cbad0e4b476c255b30b9eedacbf06
Roberto Carlos Salas Valencia <roberto.cs.valencia@gmail.com>
2024-04-14 19:35:25 -0500

    refactor: removing unused code for string VO

--- src/modules/shared/core/domain/string.value-object.ts
+++ src/modules/shared/core/domain/string.value-object.ts
@@ -1,20 +1,10 @@
 import { ValueObject } from "./value-object.abstract";
 
-interface IPrimitiveStringValueObject {
-  value?: string;
-}
-
 export class StringValueObject extends ValueObject {
   readonly value: string;
 
   constructor(value: string) {
     super(false, value);
     this.value = value;
-  }
-
-  toPrimitives(): IPrimitiveStringValueObject {
-    return {
-      value: this.value,
-    };
   }
 }
